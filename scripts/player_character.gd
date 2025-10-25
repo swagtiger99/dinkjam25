bcb174a943f6c9e0ff47d9b7768c2667ddeceeec
swagtiger99 <peacelom05@gmail.com>
2025-10-25 17:34:10 +0100

    player updates

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,25 +1,72 @@
-extends CharacterBody2D
+class_name playerCharacter extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+var HP = 7
+var ammo = 6
+var bullet_scene = preload("res://scenes/bullet.tscn")
+var reloading = false
+var damaged = false
+var inCutscene = false
+var ending = false
+
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var Walk_Timer = $Timer
+@onready var PlayerCam = $Camera2D
+@onready var RevolverSprite = $GunPivot/Revolver
+
+func playerDamaged(damageTaken):
+	HP -= damageTaken
+	
+func shoot():
+	
+	if ammo != 0:
+		var bullet = bullet_scene.instantiate()
+		get_tree().get_root().add_child(bullet)
+		
+		bullet.rotation = get_node("GunPivot").rotation
+		
+		bullet.position.y = $GunPivot/Revolver.global_position.y
+		bullet.position.x = $GunPivot/Revolver.global_position.x
+		
+		ammo -= 1
+		reloading = false
+		
+	elif ammo == 0 and reloading == false:
+		reload()
+		
+func reload():
+	reloading = true
+	ammo = 0
+	await get_tree().create_timer(2).timeout
+	ammo = 6
+	 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction = Input.get_vector("left","right","up","down")
+	velocity = direction * SPEED
+	
+	if direction.x > 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("side")
+	elif direction.x < 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("side")
+	elif direction.y != 0:
+		animated_sprite.play("vertical")
+	if direction. x == 0 and direction.y == 0:
+		animated_sprite.play("idle")
+	if inCutscene == false or ending == true:
+		move_and_slide()
+		
+	if Input.is_action_just_pressed("shoot") and ammo != 0:
+		shoot()
+	elif Input.is_action_just_pressed("reload") and ammo !=6:
+		reload()
+	
+	if HP == 0:
+		get_tree().reload_current_scene()
 
 	move_and_slide()
---
+++ scripts/revolver.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+
+@onready var pivot_node = $".."
+const rotation_speed = 500.0
+
+func _physics_process(delta):
+	pivot_node.look_at(get_global_mouse_position())
+	print(pivot_node.rotation)
+	var revolver_rotation = pivot_node.rotation
+	
+	if pivot_node.rotation_degrees > 360:
+		pivot_node.rotation_degrees = 0
+	elif pivot_node.rotation_degrees < -360:
+		pivot_node.rotation_degrees = 0
+	
+	if pivot_node.rotation_degrees <= -90 and pivot_node.rotation_degrees >= -270 or pivot_node.rotation_degrees >= 90 and pivot_node.rotation_degrees <= 270 :
+		self.flip_v = true
+	else:
+		self.flip_v = false
